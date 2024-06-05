ae120b92bb09b3848527deac871189baae987d21
Matsuuu <huhta.matias@gmail.com>
2024-06-05 19:26:55 +0300

    Add some util code

---
+++ wiimote/src/slider.js
@@ -0,0 +1,3 @@
+export function nextSlide() {
+    document.querySelector("#punch-viewer-speakernotes > table > tbody > tr > td.punch-viewer-speakernotes-side-panel > div.punch-viewer-speakernotes-page-control-panel > div > div.punch-viewer-speakernotes-page-next")?.click();
+}
